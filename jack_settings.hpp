489c011a033535bcfed701c7243fca7dbc075ebd
Jack Lewis <jack@calderadev.com>
2018-06-15 15:34:34 -0700

    Added more stuff.

---
+++ jack_settings.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Jack on 6/5/2018.
+//
+
+#ifndef MINIFEPOSIT_JACK_SETTINGS_HPP
+#define MINIFEPOSIT_JACK_SETTINGS_HPP
+
+#define MINIFE_DEBUG 1
+#define USE_POSITX 1
+#define MINIFE_FUSED 1
+#define USES_QUIRE 1
+
+
+//manipulate the size of the posit/quire
+#define UNUM_NBIT_SIZE 16
+#define UNUM_ES_SIZE 0
+#define UNUM_QUIRE_CAPACITY 10
+
+
+
+#endif //MINIFEPOSIT_JACK_SETTINGS_HPP
